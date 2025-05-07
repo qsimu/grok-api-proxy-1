9900d35db7e745f7fddffb5dc61e7f3dc1275147
斑马 <42400779+qsimu@users.noreply.github.com>
2025-05-07 13:32:23 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -41,5 +41,17 @@ function handleOptionsRequest() {
   headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   headers.set('Access-Control-Max-Age', '86400');
+  headers.delete('CF-Connecting-IP');
+  headers.delete('X-Forwarded-For');
+  headers.delete('X-Real-IP');
+  headers.delete('True-Client-IP');
+  headers.delete('x-vercel-deployment-url');
+  headers.delete('x-vercel-forwarded-for');
+  headers.delete('x-forwarded-host');
+  headers.delete('x-forwarded-port');
+  headers.delete('x-forwarded-proto');
+  headers.delete('x-vercel-id');
+  headers.delete('origin');
+  headers.delete('baggage');
   return new Response(null, { headers });
 }
